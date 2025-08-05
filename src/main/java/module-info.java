2da963aad229dc6d5e0c1bd3141514a6f68bc197
Iaroslav Tsymbaliuk <episkob@gmail.com>
2025-08-05 11:02:06 +0200

    feat: add module-info.java for modularization and improved dependency management

---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+module it.r2u.animar2u {
+    requires transitive javafx.controls;
+    requires transitive javafx.fxml;
+    requires transitive javafx.web;
+    requires transitive javafx.graphics;
+    requires transitive javafx.base;
+
+    // External libraries - могут вызывать проблемы с модулями
+    // requires org.controlsfx.controls;
+    // requires org.kordamp.ikonli.javafx;
+
+    opens it.r2u.animar2u to javafx.fxml;
+    opens it.r2u.animar2u.ui.controllers to javafx.fxml;
+    
+    exports it.r2u.animar2u;
+    exports it.r2u.animar2u.ui.controllers;
+    exports it.r2u.animar2u.core.tab_management;
+    exports it.r2u.animar2u.core.storage;
+    exports it.r2u.animar2u.core.navigation;
+    exports it.r2u.animar2u.core.config;
+    exports it.r2u.animar2u.core.initialization;
+    exports it.r2u.animar2u.media;
+}
